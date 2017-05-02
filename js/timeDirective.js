49f1357f49de6f900b4b306a756e5cfddc525d9f
Jessica Hollingshead <Jessica Hollingshead>
2017-05-01 20:15:31 -0600

    finished mini-project

---
+++ js/timeDirective.js
@@ -0,0 +1,12 @@
+angular.module('app').directive('showTime', function(){
+return {
+  restrict: 'E',   
+  template: '<div>The current time is {{time}}</div>',
+  link: function(scope, element, attributes){
+      var currentTime = new Date();
+      scope.time = currentTime.toString();
+  }
+  
+}
+
+})
